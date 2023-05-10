5845188988904643a647d68052ebc59df1a653bd
Juliocastro240902 <Juliocastro.contato@gmai.com>
2023-05-09 23:12:44 -0300

    Connection to github api to search profile informations

--- src/Pages/Home/components/Profile/index.tsx
+++ src/Pages/Home/components/Profile/index.tsx
@@ -18,49 +18,72 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import GitIcon from '../../../../assets/Github.svg'
 import BuildIcon from '../../../..//assets/Build.svg'
 import ProfileIcon from '../../../..//assets/Profile.svg'
-import Tony from '../../../..//assets/42015a20-6fb3-4370-974a-1b414be02721.jfif'
+import { useEffect, useState } from 'react'
+
+interface DataTypes {
+  id: number
+  avatar_url?: string
+  name: string
+  followers: string
+  login: string
+  company?: string
+  bio?: string
+  html_url: string
+}
 
 export function Profile() {
+  const [profile, setProfile] = useState<DataTypes>()
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/Gabriel-Duarte-Dev').then((res) => {
+      return res.json().then((data) => {
+        setProfile(data)
+      })
+    })
+  }, [setProfile])
+
+  const description = `Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+  viverra massa quam dignissim aenean malesuada suscipit. Nunc,
+  volutpat pulvinar vel mass.`
+
   return (
     <ProfileContainer>
       <ProfileModule>
         <Picture>
-          <img src={Tony} alt="" />
+          <img src={profile?.avatar_url} alt="" />
         </Picture>
 
         <ProfileInfo>
           <TitleBox>
-            <h1>Antony Amorim Eufrasio</h1>
+            <h1>{profile?.name}</h1>
             <UnderButton>
-              <button type="submit">
-                github
-                <FontAwesomeIcon
-                  icon={faArrowUpRightFromSquare}
-                  color="#3294F8"
-                  size="xs"
-                />
-              </button>
+              <a href={profile?.html_url} target="_blank" rel="noreferrer">
+                <button type="submit">
+                  github
+                  <FontAwesomeIcon
+                    icon={faArrowUpRightFromSquare}
+                    color="#3294F8"
+                    size="xs"
+                  />
+                </button>
+              </a>
             </UnderButton>
           </TitleBox>
 
-          <Descripiton>
-            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-            volutpat pulvinar vel mass.
-          </Descripiton>
+          <Descripiton>{profile?.bio ?? description}</Descripiton>
 
           <ProfileSubInfo>
             <SubInfoBox>
               <Icon src={GitIcon} alt="" />
-              <Paragraph>Cameronwll</Paragraph>
+              <Paragraph>{profile?.name}</Paragraph>
             </SubInfoBox>
             <SubInfoBox>
               <Icon src={BuildIcon} alt="" />
-              <Paragraph>Rocketseat</Paragraph>
+              <Paragraph>{profile?.company || 'Não'}</Paragraph>
             </SubInfoBox>
             <SubInfoBox>
               <Icon src={ProfileIcon} alt="" />
-              <Paragraph>32 seguidores</Paragraph>
+              <Paragraph>{profile?.followers} seguidores</Paragraph>
             </SubInfoBox>
           </ProfileSubInfo>
         </ProfileInfo>
